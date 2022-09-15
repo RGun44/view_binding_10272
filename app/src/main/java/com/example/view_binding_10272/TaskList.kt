c1be3aaaffd7af86725b855a6a95f27326662342
RGun44* <ricogunawan444@gmail.com*>
2022-09-15 14:50:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10272/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10272
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1","Bangun Tidur","Kebiasaan sehari-hari untuk membuat hari makin Indah"),
+        Task("Chapter 2","Nguli Genshin Impact","Saya tak tahu"),
+        Task("Chapter 3","Ngopi","Ngula Nguli Ngopa Ngopi"),
+        Task("Chapter 4","diamana mana Ei?","How can I be homophobic, my bitch is gay"),
+        Task("Chapter 5","Upload ke Tiktok","Pamer terus")
+    )
+}
